ce37162885a26055fce311e8274052ee457219d9
Soufiane Ezzemany <soufianezzemany@gmail.com>
2025-11-22 10:17:35 +0100

    fix: update Docker provider version to fix GPG key expiration
    Updated kreuzwerker/docker provider from ~> 3.0.1 to ~> 3.0.2
    to resolve GPG key expiration issue that was preventing terraform init.
    
    This is a known issue with the provider's signing key.

--- terraform/part1-docker/versions.tf
+++ terraform/part1-docker/versions.tf
@@ -8,9 +8,8 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0.1" # Allow patch updates (3.0.x) but not minor/major
+      version = "~> 3.0.2"
     }
   }
-  required_version = ">= 1.0" # Minimum Terraform version required
+  required_version = ">= 1.0"
 }
-
