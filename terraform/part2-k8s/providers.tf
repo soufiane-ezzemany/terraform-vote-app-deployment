c405cca81cef7f08e6442ffdbe60f3e1c9af028b
Soufiane Ezzemany <soufianezzemany@gmail.com>
2025-11-24 14:08:52 +0100

    fix: deleted wrong terraform

--- terraform/part2-k8s/providers.tf
+++
@@ -1,8 +0,0 @@
-# =============================================================================
-# Kubernetes Provider Configuration
-# =============================================================================
-
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "minikube" # Updated for local Minikube testing
-}
